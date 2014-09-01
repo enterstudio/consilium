02e5e5155745f74db19cd5208cf9371e2242a510
Cameron Dykes <yellow5c@gmail.com>
2014-09-01 15:32:00 -0500

    Add consilium_helpers:return_error/3

--- src/consilium_helpers.erl
+++ src/consilium_helpers.erl
@@ -1,6 +1,6 @@
 -module(consilium_helpers).
 
--export([formatted_date/1, extract_value_from_json/2]).
+-export([formatted_date/1, extract_value_from_json/2, return_error/3]).
 
 formatted_date({Year,Month,Day}) ->
     Y = integer_to_list(Year),
@@ -13,3 +13,7 @@ extract_value_from_json(Key, JsonBody) ->
         {_, {_, Value}} -> Value;
         _ -> false
     end.
+
+return_error(Msg, ReqData, State) ->
+    Error = mochijson:encode({struct, [ {"error", Msg} ]}),
+    {{halt, 400}, wrq:append_to_response_body(Error, ReqData), State}.
