d2b42e2a118932c715e5fbad0070b959f35829c5
Cameron Dykes <yellow5c@gmail.com>
2014-09-01 17:27:32 -0500

    Update credit_resource to use consilium_stats:credit_sale/1

--- src/credit_resource.erl
+++ src/credit_resource.erl
@@ -3,18 +3,33 @@
 %% @doc credit webmachine_resource.
 
 -module(credit_resource).
--export([init/1, content_types_provided/2, to_json/2]).
+-export([init/1, allowed_methods/2, content_types_provided/2]).
+-export([process_post/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
+allowed_methods(ReqData, State) ->
+    {['HEAD','POST'], ReqData, State}.
+
 content_types_provided(ReqData, State) ->
     {[ {"application/json", to_json} ], ReqData, State}.
 
-to_json(ReqData, State) ->
-    Resp = mochijson:encode({struct, [
-                                        {id, 3}
-                                     ]}),
-    {Resp, ReqData, State}.
+process_post(ReqData, State) ->
+    case extract_request_values(ReqData) of
+        {false,_} ->
+            consilium_helpers:return_error("Missing required webmaster_id", ReqData, State);
+        {_,false} ->
+            consilium_helpers:return_error("Missing required widget_id", ReqData, State);
+        {WebmasterId, WidgetId} ->
+            consilium_stats:credit_sale({WebmasterId, WidgetId}),
+            Resp = mochijson:encode({struct, [ {"webmaster_id", WebmasterId} ]}),
+            {true, wrq:append_to_response_body(Resp, ReqData), State}
+    end.
 
+extract_request_values(ReqData) ->
+    {_, JsonBody} = mochijson:decode(wrq:req_body(ReqData)),
+    WebmasterId = consilium_helpers:extract_value_from_json("webmaster_id", JsonBody),
+    WidgetId = consilium_helpers:extract_value_from_json("widget_id", JsonBody),
+    {WebmasterId, WidgetId}.
