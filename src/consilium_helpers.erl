bed46b889770b40becfba3748d2fc336413b5e35
Cameron Dykes <yellow5c@gmail.com>
2014-09-01 15:28:25 -0500

    Add consilium_helpers:extract_value_from_json/2

--- src/consilium_helpers.erl
+++ src/consilium_helpers.erl
@@ -1,9 +1,15 @@
 -module(consilium_helpers).
 
--export([formatted_date/1]).
+-export([formatted_date/1, extract_value_from_json/2]).
 
 formatted_date({Year,Month,Day}) ->
     Y = integer_to_list(Year),
     M = integer_to_list(Month),
     D = integer_to_list(Day),
     Y ++ "-" ++ M ++ "-" ++ D.
+
+extract_value_from_json(Key, JsonBody) ->
+    case lists:keysearch(Key, 1, JsonBody) of
+        {_, {_, Value}} -> Value;
+        _ -> false
+    end.
