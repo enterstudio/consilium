86ca92bd2ba419bca318d5d5aa5694c6f6e30bb9
Cameron Dykes <yellow5c@gmail.com>
2014-09-01 15:24:58 -0500

    Update clicks_resource to handle the initial post request

--- src/clicks_resource.erl
+++ src/clicks_resource.erl
@@ -3,17 +3,45 @@
 %% @doc clicks webmachine_resource.
 
 -module(clicks_resource).
--export([init/1, content_types_provided/2, to_json/2]).
+-export([init/1, allowed_methods/2, content_types_provided/2]).
+-export([process_post/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
+allowed_methods(ReqData, State) ->
+    {['HEAD','POST'], ReqData, State}.
+
 content_types_provided(ReqData, State) ->
     {[ {"application/json", to_json} ], ReqData, State}.
 
-to_json(ReqData, State) ->
-    Resp = mochijson:encode({struct, [
-                                        {id, 2}
-                                     ]}),
-    {Resp, ReqData, State}.
+process_post(ReqData, State) ->
+    case extract_request_values(ReqData) of
+        {false,_} ->
+            Error = mochijson:encode({struct, [
+                                               {"error",
+                                                "Missing required webmaster_id"}]}),
+            {{halt, 400}, wrq:append_to_response_body(Error, ReqData), State};
+        {_,false} ->
+            Error = mochijson:encode({struct, [
+                                               {"error",
+                                                "Missing required widget_id"}]}),
+            {{halt, 400}, wrq:append_to_response_body(Error, ReqData), State};
+        {WebmasterId, WidgetId} ->
+            consilium_stats:track_click({WebmasterId, WidgetId}),
+            Resp = mochijson:encode({struct, [ {"webmaster_id", WebmasterId} ]}),
+            {true, wrq:append_to_response_body(Resp, ReqData), State}
+    end.
+
+extract_request_values(ReqData) ->
+    {_, JsonBody} = mochijson:decode(wrq:req_body(ReqData)),
+    WebmasterId = extract_value_from_json("webmaster_id", JsonBody),
+    WidgetId = extract_value_from_json("widget_id", JsonBody),
+    {WebmasterId, WidgetId}.
+
+extract_value_from_json(Key, JsonBody) ->
+    case lists:keysearch(Key, 1, JsonBody) of
+        {_, {_, Value}} -> Value;
+        _ -> false
+    end.
